d651021c8b019077626ab4a638dc9df953d06b38
Kenta Ishiguro <kentaishiguro@slowstart.org>
2025-05-22 11:01:37 +0200

    Update

--- profile.py
+++ profile.py
@@ -1,10 +1,13 @@
-"""This profile sets up a simple NFS server and a network of clients. The NFS server uses 
-*temporary* storage on one of your nodes, the contents will be lost when you terminate your
-experiment. We have a different profile available if you need your NFS server data to
-persist after your experiment is terminated. 
+"""This profile sets up a simple environment for migration experiment with KVM.
+Two servers can share a VM image through NFS. To do so, one server acts as NFS
+server and the other one acts as NFS client. The NFS server uses *temporary*
+storage on one of your nodes, the contents will be lost when you terminate your
+experiment. We have a different profile available if you need your NFS server
+data to persist after your experiment is terminated. 
 
 Instructions:
-Click on any node in the topology and choose the `shell` menu item. Your shared NFS directory is mounted at `/nfs` on all nodes."""
+Click on any node in the topology and choose the `shell` menu item. Your shared
+NFS directory is mounted at `/nfs` on all nodes."""
 
 # Import the Portal object.
 import geni.portal as portal
@@ -19,22 +22,9 @@ pc = portal.Context()
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
 
-# Client image list
+# Image list
 imageList = [
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', 'UBUNTU 20.04'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD', 'UBUNTU 18.04'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS8-64-STD', 'CENTOS 8'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS7-64-STD', 'CENTOS 7'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//FBSD131-64-STD', 'FreeBSD 13.1'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//FBSD123-64-STD', 'FreeBSD 12.3'),
-]
-
-# Server image list, not tested with CentOS
-imageList2 = [
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', 'UBUNTU 20.04'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD', 'UBUNTU 18.04'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//FBSD131-64-STD', 'FreeBSD 13.1'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//FBSD123-64-STD', 'FreeBSD 12.3'),
+    ('urn:publicid:IDN+emulab.net+image+DLOCK:migration-kvm', 'UBUNTU 24.04 (kvm-qemu-libvirt)'),
 ]
 
 # Do not change these unless you change the setup scripts too.
@@ -42,20 +32,18 @@ nfsServerName = "nfs"
 nfsLanName    = "nfsLan"
 nfsDirectory  = "/nfs"
 
-# Number of NFS clients (there is always a server)
-pc.defineParameter("clientCount", "Number of NFS clients",
-                   portal.ParameterType.INTEGER, 2)
+pc.defineParameter("firstServerType", "First server type",
+                   portal.ParameterType.STRING, 'd710')
+
+pc.defineParameter("secondServerType", "Second server type",
+                   portal.ParameterType.STRING, 'd710')
 
 pc.defineParameter("osImage", "Select OS image for clients",
                    portal.ParameterType.IMAGE,
                    imageList[0], imageList)
 
-pc.defineParameter("osServerImage", "Select OS image for server",
-                   portal.ParameterType.IMAGE,
-                   imageList2[0], imageList2)
-
 pc.defineParameter("nfsSize", "Size of NFS Storage",
-                   portal.ParameterType.STRING, "200GB",
+                   portal.ParameterType.STRING, "10GB",
                    longDescription="Size of disk partition to allocate on NFS server")
 
 # Always need this when using parameters
@@ -63,29 +51,34 @@ params = pc.bindParameters()
 
 # The NFS network. All these options are required.
 nfsLan = request.LAN(nfsLanName)
+nfsLan.bandwidth = 100000
 nfsLan.best_effort       = True
 nfsLan.vlan_tagging      = True
 nfsLan.link_multiplexing = True
 
-# The NFS server.
+# The NFS server. (first server)
 nfsServer = request.RawPC(nfsServerName)
-nfsServer.disk_image = params.osServerImage
-# Attach server to lan.
-nfsLan.addInterface(nfsServer.addInterface())
+nfsServer.disk_image = params.osImage
+nfsServer.hardware_type = params.firstServerType
+nfsServer.routable_control_ip = True
+iface0 = nfsServer.addInterface('interface-0', pg.IPv4Address('192.168.6.2', '255.255.255.0'))
 # Storage file system goes into a local (ephemeral) blockstore.
 nfsBS = nfsServer.Blockstore("nfsBS", nfsDirectory)
 nfsBS.size = params.nfsSize
 # Initialization script for the server
 nfsServer.addService(pg.Execute(shell="sh", command="sudo /bin/bash /local/repository/nfs-server.sh"))
 
-# The NFS clients, also attached to the NFS lan.
-for i in range(1, params.clientCount+1):
-    node = request.RawPC("node%d" % i)
-    node.disk_image = params.osImage
-    nfsLan.addInterface(node.addInterface())
-    # Initialization script for the clients
-    node.addService(pg.Execute(shell="sh", command="sudo /bin/bash /local/repository/nfs-client.sh"))
-    pass
+# The NFS client. (second server)
+nfsClient = request.RawPC("client")
+nfsClient.disk_image = params.osImage
+nfsClient.hardware_type = params.secondServerType
+nfsClient.routable_control_ip = True
+iface0 = nfsServer.addInterface('interface-0', pg.IPv4Address('192.168.6.3', '255.255.255.0'))
+nfsClient.addService(pg.Execute(shell="sh", command="sudo /bin/bash /local/repository/nfs-client.sh"))
+
+# Attach server to lan.
+nfsLan.addInterface(nfsServer.addInterface())
+nfsLan.addInterface(nfsClient.addInterface())
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
