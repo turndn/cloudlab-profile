5ec9fd9b1194588ad21e657be2095116c596875f
Kenta Ishiguro <kentaishiguro@slowstart.org>
2025-05-23 17:28:37 +0200

    fix

--- migrate_libvirt.sh
+++ migrate_libvirt.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 sudo virsh start domain1
-sudo virsh migrate --live domain1 qemu+ssh://"$1"/system | tee ~/results/migration_"$1".txt 2>&1 || sudo virsh destroy domain1
+sudo virsh migrate --live domain1 qemu+ssh://"$1"/system 2>&1 | tee ~/results/migration_"$1".txt || sudo virsh destroy domain1
