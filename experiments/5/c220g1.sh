a7428409a1646488f836c966a5a29b062b3317de
Kenta Ishiguro <kentaishiguro@slowstart.org>
2025-05-23 13:16:17 +0200

    Set exp-5

---
+++ experiments/5/c220g1.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+/local/repository/base.sh
+
+echo "Wait"
+read hello
+/local/repository/migrate_libvirt.sh c220g2
+
+echo "Wait"
+read hello
+/local/repository/migrate_libvirt.sh d430
+
+echo "Wait"
+read hello
+/local/repository/migrate_libvirt.sh m510
---
+++ experiments/5/d430.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+/local/repository/base.sh
+
+echo "Wait"
+read hello
+/local/repository/migrate_libvirt.sh c220g1
+
+echo "Wait"
+read hello
+/local/repository/migrate_libvirt.sh c220g2
+
+echo "Wait"
+read hello
+/local/repository/migrate_libvirt.sh m510
