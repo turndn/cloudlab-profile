09e21247c41fe004680d9b85ada0f9a214173384
Kenta Ishiguro <kentaishiguro@slowstart.org>
2025-05-27 15:48:37 +0200

    Change bandwidth

--- profile.py
+++ profile.py
@@ -66,7 +66,7 @@ for client_config in clients:
                                       pg.IPv4Address(client_config.ipaddr, '255.255.255.0')))
 
 lan = request.LAN("lan")
-lan.bandwidth = 50000
+lan.bandwidth = 10000
 
 # Attach server to lan.
 for iface in ifaces:
